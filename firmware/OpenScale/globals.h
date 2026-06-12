cc2d7f4cb520e5b67f736364543e15f404b6d586
Olivia Smith <osmith@nicindustries.com>
2026-06-12 07:55:30 -0700

    Added more definitionas to globals.h

--- firmware/OpenScale/globals.h
+++ firmware/OpenScale/globals.h
@@ -6,7 +6,6 @@
 //#define USING_TTL
 
 // global defaults for Flow Rate Tester OpenScale
-
 const long UART_SPEED = 115200;
 const byte UNITS = 0;
 const long TARE_POINT = 1000;
@@ -15,7 +14,9 @@ const byte DECIMAL_PLACES = 4;
 const byte AVERAGE_AMOUNT = 63;
 const byte STATUS_ENABLE = true;
 const byte ESCAPE_CHAR = 'x';
+const int MIN_POWERCYCLE_TIME = 500;
 
+// global methods
 
 
 
