3673052cd147d2327f6ab3c1d8dee40113d854fe
Olivia Smith <osmith@nicindustries.com>
2026-06-12 07:30:05 -0700

    Added more definitions to globals.h

--- firmware/OpenScale/globals.h
+++ firmware/OpenScale/globals.h
@@ -2,6 +2,21 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#define USING_USB
+//#define USING_TTL
+
+// global defaults for Flow Rate Tester OpenScale
+
+const long UART_SPEED = 115200;
+const byte UNITS = 0;
+const long TARE_POINT = 1000;
+const uint8_t TIMESTAMP_ENABLE = false;
+const byte DECIMAL_PLACES = 4;
+const byte AVERAGE_AMOUNT = 63;
+const byte STATUS_ENABLE = true;
+const byte ESCAPE_CHAR = 'x';
+
+
 
 
 #endif
